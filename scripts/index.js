42ebe826e93f8927ab2f0c7c434e49c4220471e2
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-05-27 22:32:53 +0300

    init

--- scripts/index.js
+++ scripts/index.js
@@ -1,185 +1,135 @@
 "use strict";
 
-// HOMEWORK #20
+// HOMEWORK #21
 
 //1//
-let arr1 = [1,2,3];
-let arr2 = [4,5,6];
+console.log("Task#1");
 
-let concatArr1Arr2 = arr1.concat(arr2);
-console.log(concatArr1Arr2);
+
+const arr1 = ["a", "b", "c", "d"];
+const firstSum = arr1[0] + "+" + arr1[1];
+const secondSum = arr1[2] + "+" + arr1[arr1.length-1];
+console.log(firstSum, secondSum);
 
 //2//
-let arr3 = [1,2,3];
-arr3.reverse();
-console.log(arr3);
+console.log("Task#2");
 
-//3//
+const arr2 = [2, 5, 3, 9];
+const firstMul = arr2[0] * arr2[1];
+const secondMul = arr2[2] * arr2[3];
+const result = firstMul + secondMul;
 
-let arr4 = [1,2,3];
-arr4.push(4,5,6);
-console.log(arr4);
-
-//4//
-
-let arr5 = [1,2,3];
-arr5.unshift(4,5,6);
-console.log(arr5);
-
-//5//
-
-let arr6 =  ['js', 'css', 'jq'];
-console.log(arr6[0]);
-
-//6//
-
-let arr7 =  ['js', 'css', 'jq'];
-// console.log(arr7[arr7.length-1]);
-let arr71 = arr7.pop();
-console.log(arr71);
-
-//7//
-
-let arr8 =  [1,2,3,4,5];
-
-let arr9 = arr8.slice(0, 3);
-console.log(arr9);
-
-//8//
-
-let arr10 =  [1,2,3,4,5];
-
-let arr11 = arr10.slice(-2);
-console.log(arr11);
-
-//9//
-
-let arr12 =  [1,2,3,4,5];
-arr12.splice(1,2);
-console.log(arr12);
-
-//10//
-
-let arr13 =  [1,2,3,4,5];
-let arr14 = arr13.slice(1,4);
-console.log(arr14);
-
-//11//
-
-let arr15 =  [1,2,3,4,5];
-arr15.splice(2, 0, "a", "b", "c");
-console.log(arr15);
-
-//12//
-
-let arr16 =  [1,2,3,4,5];
-arr16.splice(1, 0, "a", "b");
-arr16.splice(6,0, "c");
-arr16.push("e");
-console.log(arr16);
-
-//13//
-
-let arr17 =  [3,4,1,2,7];
-arr17.sort(compareNumeric);
-
-function compareNumeric(a, b) {
-    // alert( a + " <> " + b )
-    if (a > b) return 1;   // если первое значение больше второго
-    if (a === b) return 0; // если равны
-    if (a < b) return -1
-
-}
-console.log(arr17);
-
-//14//
-
-let arr18 =  [5,6,7,8,9];
-
-const result = arr18.reduce(
-    function (accum, item) {
-        accum += item;
-        return accum;
-    },
-    0
-)
 console.log(result);
 
+//3//
+console.log("Task#3");
 
-//15//
+const arr3 = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+console.log(arr3[1][0]);
 
-let arr19 =  [5,6,7,8,9];
+//4//
+console.log("Task#4");
 
-const result2 = arr19.reduce(
+const obj1 = {
+    js: ["jQuery", "Angular"],
+    php: "hello",
+    css: "world"};
+
+console.log(obj1.js[0]);
+
+
+//5//
+console.log("Task#5");
+
+const arr5= [];
+let arrayX = "";
+
+for (let i = 0; i < 5; i++) {
+    arr5.push(arrayX);
+    arrayX += "x";
+}
+
+console.log(arr5);
+
+
+//6//
+console.log("Task#6");
+
+const arr6 = [];
+let num1 = 0;
+
+for (let i = 1; i < 5; i++) {
+    num1 = "";
+    for (let x = 1; x <= i; x++) {
+        num1 += i;
+    }
+    arr6.push(num1);
+}
+console.log(arr6);
+
+//7//
+console.log("Task#7");
+
+function fun1 (item, arrayLength) {
+ let arr7 = [];
+for (let i = 0; i <= arrayLength-1; i++) {
+    arr7.push(item);
+}
+ return arr7;
+}
+console.log(fun1("x", 5));
+
+//8//
+console.log("Task#8");
+
+let arr8 = [1, 2, 3, 4, 5, 6, 7];
+let sum = 0;
+for (let i = 0; i < arr8.length; i++) {
+   sum += arr8[i];
+    if (sum >= 10) {
+        console.log(i+1);
+        break;
+    }
+}
+
+//9//
+console.log("Task#9");
+
+let arr9 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let arr9Red = arr9.reduce(
     function (accum, item) {
-        accum.push (item ** 2);
+        accum.unshift(item);
         return accum;
     },
     []
-)
-console.log(result2);
+);
+console.log(arr9Red);
 
-//16//
+//10//
+console.log("Task#10");
 
-let arr20 =  [1,-3, 5, 6,-7, 8, 9,-11];
+let arr10 = [[1, 2, 3], [4, 5], [6], 21];
+let arr10Flat = arr10.flat();
 
-const result3 = arr20.filter(filterFuncNeg);
-console.log(result3);
-
-function filterFuncNeg (item) {
-    return item < 0;
-
-}
-
-//17//
-
-let arr21 =  [1,-3, 5, 6,-7, 8, 9,-11];
-
-const result4 = arr21.filter(filterFuncEven);
-console.log(result4);
-
-function filterFuncEven (item) {
-    return item % 2 === 0;
-
-}
-
-//18//
-
-let arr22 = ['aaa', 'aaaqqq', 'zzzqq', 'zz', 'qsaa', 'q', 'az'];
-
-const result5 = arr22.filter(stringLength);
-
-function stringLength (item) {
-    return item.length >= 5;
-}
-console.log(result5)
-
-
-//19//
-
-let arr23 = [1, 2, [3, 4], 5, [6, 7]];
-
-const result6 = arr23.reduce(
+let arr10FlatRed = arr10Flat.reduce(
     function (accum, item) {
-        if (Array.isArray(item)) {
-            accum.push(item)}
+        accum +=item;
         return accum;
     },
-    []
-)
-console.log(result6)
+);
+console.log(arr10FlatRed);
 
+//11//
+console.log("Task#11");
 
-//20//
+let arr11 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+let arr11Flat = arr11.flat(2);
 
-let arr24 =  [5,-3, 6,-5, 0,-7, 8, 9];
-
-const result7 = arr24.reduce(
+let arr11FlatRed = arr11Flat.reduce(
     function (accum, item) {
-        if (item < 0) {
-        accum++}
-        return accum;
-    },
-    0
-)
-console.log(result7);
+    accum += item;
+    return accum;
+},
+    );
+console.log(arr11FlatRed);
